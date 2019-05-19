2ab81410a3b285c4f61ebcde484662dae94e7a10
Merwann Ayed <37482556+secret-merwann@users.noreply.github.com>
2019-05-19 18:24:09 +0200

    First commit

---
+++ TP01.2 - IMC GUI/Form1.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace TP01._2___IMC_GUI
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent() {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.poidsTxtBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.tailleTxtBox = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.boutonCalculerIMC = new System.Windows.Forms.Button();
+            this.label11 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.label1.Location = new System.Drawing.Point(70, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(290, 33);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Calcul de votre IMC";
+            // 
+            // poidsTxtBox
+            // 
+            this.poidsTxtBox.Location = new System.Drawing.Point(120, 347);
+            this.poidsTxtBox.Name = "poidsTxtBox";
+            this.poidsTxtBox.Size = new System.Drawing.Size(199, 20);
+            this.poidsTxtBox.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(73, 53);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(325, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "A l’heure actuelle, l’IMC est l’indicateur le plus utile des risques pour";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(73, 66);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(298, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "la santé associés à un poids excessif et à un poids insuffisant.";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.label4.Location = new System.Drawing.Point(62, 96);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(336, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "L’indice de masse corporelle (IMC) est une valeur qui permet d’estimer";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.label5.Location = new System.Drawing.Point(62, 109);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(334, 13);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "la corpulence d’une personne. Ce calcul prend en compte à la fois la ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.BackColor = System.Drawing.Color.PaleVioletRed;
+            this.label6.Location = new System.Drawing.Point(62, 122);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(124, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "taille, et à la fois le poids.";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(72, 320);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(297, 24);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Votre poids : (en kilogrammes)";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.BackgroundImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(136, 138);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(183, 179);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.Color.Transparent;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label8.Location = new System.Drawing.Point(73, 370);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(68, 13);
+            this.label8.TabIndex = 9;
+            this.label8.Text = "Exemple : 45";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(74, 392);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(279, 24);
+            this.label9.TabIndex = 10;
+            this.label9.Text = "Votre taille : (en centimètres)";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // tailleTxtBox
+            // 
+            this.tailleTxtBox.Location = new System.Drawing.Point(120, 419);
+            this.tailleTxtBox.Name = "tailleTxtBox";
+            this.tailleTxtBox.Size = new System.Drawing.Size(199, 20);
+            this.tailleTxtBox.TabIndex = 11;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.BackColor = System.Drawing.Color.Transparent;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.label10.Location = new System.Drawing.Point(75, 442);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(74, 13);
+            this.label10.TabIndex = 12;
+            this.label10.Text = "Exemple : 150";
+            // 
+            // boutonCalculerIMC
+            // 
+            this.boutonCalculerIMC.Location = new System.Drawing.Point(76, 472);
+            this.boutonCalculerIMC.Name = "boutonCalculerIMC";
+            this.boutonCalculerIMC.Size = new System.Drawing.Size(291, 57);
+            this.boutonCalculerIMC.TabIndex = 13;
+            this.boutonCalculerIMC.Text = "Calculer mon IMC";
+            this.boutonCalculerIMC.UseVisualStyleBackColor = true;
+            this.boutonCalculerIMC.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.Location = new System.Drawing.Point(376, 532);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(60, 9);
+            this.label11.TabIndex = 14;
+            this.label11.Text = "AYED Merwann";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Pink;
+            this.ClientSize = new System.Drawing.Size(437, 541);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.boutonCalculerIMC);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.tailleTxtBox);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.poidsTxtBox);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calcul de l\'IMC";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox poidsTxtBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.TextBox tailleTxtBox;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Button boutonCalculerIMC;
+        private System.Windows.Forms.Label label11;
+    }
+}
+
---
+++ TP01.2 - IMC GUI/Form1.cs
@@ -0,0 +1,92 @@
+﻿/* 
+ * 
+ * Auteur : AYED Merwann 
+ * 
+ * 
+ */
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TP01._2___IMC_GUI
+{
+    public partial class Form1 : Form
+    {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void label9_Click(object sender, EventArgs e) {
+
+        }
+
+        // Algorithme de calcul de l'IMC
+        static void calculIMC(double poids, double taille) {
+
+            // Je convertis et traite les calculs au début
+            double taillenew = (taille / 100);
+            double IMCcalcul = poids / Math.Pow(taillenew, 2);
+
+            // Arrondi pour faire plus propre
+            double IMC = Math.Round(IMCcalcul, 2);
+
+
+            // Algorithme de sélection de l'option selon la valeur de l'IMC
+            if (IMC > 40) {
+
+                MessageBox.Show("Votre IMC est de " + IMC + "\nVous êtes en situation d'obésité morbide.");
+
+            } else if (IMC >= 35 && IMC <= 40) {
+
+                MessageBox.Show("Votre IMC est de " + IMC + "\nVous êtes en situation d'obésité sévère.");
+
+            } else if (IMC >= 30 && IMC <= 35) {
+
+                MessageBox.Show("Votre IMC est de " + IMC + "\nVous êtes en situation d'obésité modérée.");
+
+            } else if (IMC >= 25 && IMC <= 30) {
+
+                MessageBox.Show("Votre IMC est de " + IMC + "\nVous êtes en situation de surpoids.");
+
+            } else if (IMC >= 18.5 && IMC <= 25) {
+                
+                MessageBox.Show("Votre IMC est de " + IMC + "\nVous êtes en situation de corpulence normale.");
+
+            } else if (IMC >= 16.5 && IMC <= 18.5) {
+
+                MessageBox.Show("Votre IMC est de " + IMC + "\nVous êtes en situation de maigreur.");
+
+            } else {
+                MessageBox.Show("Votre IMC est de " + IMC + "\nVous êtes en situation de famine.");
+
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            // Utilisation du try catch pour un gain de sécurité
+
+            try {
+
+                // Hop, je convertis pour éviter les erreurs
+                double poids = Convert.ToDouble(poidsTxtBox.Text);
+                double taille = Convert.ToDouble(tailleTxtBox.Text);
+
+                // Je me suis arrangé pour enclencher la fonction au moment du clic sur le bouton afin d'aérer le code
+                calculIMC(poids, taille);
+
+            } catch {
+
+                // Message d'erreur au cas où
+                MessageBox.Show("Une erreur a été rencontrée, veuillez vérifier si le format des données saisies ont été respectées.", "Erreur !",
+   MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
